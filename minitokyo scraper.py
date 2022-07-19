1e2e2246917fef08df034676d232bc97a4232483
HeWhoIsLearningToCode <esparrag0@protonmail.com>
2022-07-19 15:13:18 -0700

    Update minitokyo scraper.py

--- minitokyo scraper.py
+++ minitokyo scraper.py
@@ -1,21 +1,42 @@
 import requests, bs4, sys, re
 from pathlib import Path
 
+def link_getter(res_link, link_segment):    
+    res = requests.get(res_link)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.content, 'lxml')
+    links = soup.select('a')
+    all_links = []
+    for link in links:
+        if link_segment in str(link):
+            all_links.append(link)
+    return all_links
+
+
 res = requests.get('http://www.minitokyo.net/Darling in the FranXX')
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.content, 'lxml')
 links = soup.select('a')
-images_links = []
+all_links = []
 for link in links:
     if 'http://browse.minitokyo.net/gallery/?tid=' in str(link):
-        images_links.append(link)
-url = re.compile(r'http(.*)index=3')
-scans_link = url.search(str(images_links[-1])).group()
-scans_link = scans_link.split('amp;')
-scans_link = scans_link[0] + scans_link[-1]
+        all_links.append(link)
+scans_link = all_links[-1].get('href')
 
 res = requests.get(scans_link)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.content, 'lxml')
-images_links = soup.select('a')
+all_links = soup.select('a')
+links_list = []
+for link in all_links:
+    if 'http://gallery.minitokyo.net/view/' in str(link):
+        image_link = link.get('href')
+        res = requests.get(image_link)
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.content, 'lxml')
+        all_links = soup.select('a')
+    if 'index=3&page=' in str(link):
+        links_list.append(link)
+
+
 
