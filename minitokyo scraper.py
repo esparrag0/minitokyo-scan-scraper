e2c8c72a994e06e94e36eae8447ebdce064015a4
HeWhoIsLearningToCode <esparrag0@protonmail.com>
2022-07-19 16:12:06 -0700

    Update minitokyo scraper.py

--- minitokyo scraper.py
+++ minitokyo scraper.py
@@ -15,15 +15,14 @@ def link_getter(res_link, link_segment):
 scans_link = link_getter('http://www.minitokyo.net/Darling+in+the+FranXX', 'http://browse.minitokyo.net/gallery/?tid=')[-1].get('href')
 image_link = link_getter(str(scans_link), 'http://gallery.minitokyo.net/view/')
 for i, link in enumerate(image_link):
-    image_link[i] = image_link[i].get('href')
+    image_link[i] = link.get('href')
 
 download_links = []
 for link in image_link:
-    download_links.append(link_getter(str(link), 'http://static.minitokyo.net/downloads/'))
-for i, link in enumerate(download_links):
-    download_links[i][0] = download_links[i][0].get('href')
+    download_links.append(link_getter(str(link), 'http://static.minitokyo.net/downloads/')[0].get('href'))
+
 print(download_links)
-    # for i, link in enumerate(image_link):
-    #     image_link[i] = image_link[i].get('href')
+for image in download_links:
+
 
 
