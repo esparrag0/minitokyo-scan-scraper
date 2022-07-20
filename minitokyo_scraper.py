ed0e959203f3c91b1d2969a1f1e8529f15ced5e3
HeWhoIsLearningToCode <esparrag0@protonmail.com>
2022-07-20 00:04:42 -0700

    Update minitokyo_scraper.py

--- minitokyo_scraper.py
+++ minitokyo_scraper.py
@@ -1,4 +1,4 @@
-import requests, bs4, sys, os
+import requests, bs4, sys, os, re
 
 def link_getter(res_link, link_segment):    
     res = requests.get(res_link)
@@ -11,24 +11,36 @@ def link_getter(res_link, link_segment):
             all_links.append(link)
     return all_links
 
+def image_downloader(image_link):   
+    for i, link in enumerate(image_link):
+        image_link[i] = link.get('href')
+
+    download_links = []
+    for link in image_link:
+        download_links.append(link_getter(str(link), 'http://static.minitokyo.net/downloads/')[0].get('href'))
+
+    for image in download_links:
+        res = requests.get(image)
+        res.raise_for_status()
+        imageFile = open(os.path.join(sys.argv[1], os.path.basename(image)),'wb')
+        # Sets the pathfile in the .\sys.argv[1] directory and the filename as the basename of the image link.
+
+        for chunk in res.iter_content(100000):
+            imageFile.write(chunk)
+        imageFile.close()
+
 os.chdir(r'C:\Users\betit\Documents\pics')
-os.makedirs(sys.argv[1])
+os.makedirs(sys.argv[1], exist_ok=True)
 scans_link = link_getter('http://www.minitokyo.net/' + sys.argv[1], 'http://browse.minitokyo.net/gallery/?tid=')[-1].get('href')
-image_link = link_getter(str(scans_link), 'http://gallery.minitokyo.net/view/')
-for i, link in enumerate(image_link):
-    image_link[i] = link.get('href')
+res = requests.get(scans_link)
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.content, 'lxml')
 
-download_links = []
-for link in image_link:
-    download_links.append(link_getter(str(link), 'http://static.minitokyo.net/downloads/')[0].get('href'))
-
-for image in download_links:
-    res = requests.get(image)
-    res.raise_for_status()
-    imageFile = open(os.path.join(sys.argv[1], os.path.basename(image)),'wb')
-    # Sets the pathfile in the .\sys.argv[1] directory and the filename as the basename of the image link.
-
-    for chunk in res.iter_content(100000):
-        imageFile.write(chunk)
-    imageFile.close()
-
+page_range = str(soup.select('span[style="margin-right: 1em; "]')[0].getText)
+page_regex = re.compile(r'">(.*)\n<i>')
+page_range = page_regex.search(page_range).group(1)
+last_page = page_range.split(' ')[-1]
+for i in range(1, int(last_page) + 1):
+    page_link = scans_link + '&page=' + str(i)
+    image_link = link_getter(str(page_link), 'http://gallery.minitokyo.net/view/')
+    image_downloader(image_link)
