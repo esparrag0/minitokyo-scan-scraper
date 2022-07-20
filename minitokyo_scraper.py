d93919ac567b5f571a9c6466b5f122d0f838bbc3
HeWhoIsLearningToCode <esparrag0@protonmail.com>
2022-07-19 21:21:54 -0700

    .
    .

--- minitokyo_scraper.py
+++ minitokyo_scraper.py
@@ -11,8 +11,9 @@ def link_getter(res_link, link_segment):
             all_links.append(link)
     return all_links
 
+os.chdir(r'C:\Users\betit\Documents\pics')
 os.makedirs(sys.argv[1])
-scans_link = link_getter('http://www.minitokyo.net/Darling+in+the+FranXX', 'http://browse.minitokyo.net/gallery/?tid=')[-1].get('href')
+scans_link = link_getter('http://www.minitokyo.net/' + sys.argv[1], 'http://browse.minitokyo.net/gallery/?tid=')[-1].get('href')
 image_link = link_getter(str(scans_link), 'http://gallery.minitokyo.net/view/')
 for i, link in enumerate(image_link):
     image_link[i] = link.get('href')
@@ -24,7 +25,7 @@ for link in image_link:
 for image in download_links:
     res = requests.get(image)
     res.raise_for_status()
-    imageFile = open(os.path.join('Darling in the Franxx', os.path.basename(image)),'wb')
+    imageFile = open(os.path.join(sys.argv[1], os.path.basename(image)),'wb')
     # Sets the pathfile in the .\sys.argv[1] directory and the filename as the basename of the image link.
 
     for chunk in res.iter_content(100000):
